e749c53dd965ec221da4f3b9f1bce0313de9dfa9
HHaikal <haikalhikmi13@gmail.com>
2025-08-31 01:25:38 +0700

    feat: add auto-generation and relationship management for Sequelize models
    - Introduced AutoBuilder, AutoGenerator, AutoRelater, and AutoWriter classes to automate model creation and relationships.
    - Implemented dialect handling for various databases (MSSQL, MySQL, PostgreSQL, SQLite).
    - Enhanced TableData structure to include foreign keys and relationships.
    - Updated .gitignore to exclude unnecessary files.
    
    Signed-off-by: HHaikal <haikalhikmi13@gmail.com>

--- src/dialects/dialects.ts
+++ src/dialects/dialects.ts
@@ -5,13 +5,21 @@ import { sqliteOptions } from "./sqlite";
 import { DialectOptions } from "./dialect-options";
 import { Dialect } from "sequelize";
 
-export const dialects: { [name in Dialect]: DialectOptions | null } = {
+const dialects: { [name in Dialect]: DialectOptions | null } = {
+  db2: null,
+  oracle: null,
+  snowflake: null,
   mssql: mssqlOptions,
   mysql: mysqlOptions,
   mariadb: mysqlOptions,
   postgres: postgresOptions,
-  sqlite: sqliteOptions,
-  oracle: null,
-  db2: null,
-  snowflake: null
+  sqlite: sqliteOptions
 };
+
+export function getDialect(dialectName: Dialect) : DialectOptions {
+  const result = dialects[dialectName];
+  if(!result) {
+    throw new Error(`Dialect not available for ${dialectName}`);
+  }
+  return result as DialectOptions;
+}
